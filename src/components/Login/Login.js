762180c87fe4062016af47cdffe9d78ff05d721c
its-kayes <kayes.ek8@gmail.com>
2022-04-11 10:49:04 +0600

    Login and Sign Up update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,28 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 const Login = () => {
+    let navigate = useNavigate();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    
+
+    let [email, setEmail] = useState('');
+    let [pass, setPass] = useState('');
+
+    let emailField = e => {
+        setEmail(e.target.value);
+    }
+    let passField = e => {
+        setPass(e.target.value);
+    }
+    let submit = e => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, pass);
+    }
+
+    if (user) {
+        navigate('/shop')
+    }
+
+
+
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'> Login </h2>
                 <div>
-                    <form >
+                    <form onSubmit={submit} >
                         <div className='input-group'>
                             <label htmlFor='email'> Email </label>
-                            <input type="email" name='email' id='' required />
+                            <input onBlur={emailField} type="email" name='email' id='1' required />
                         </div>
                         <div className='input-group'>
                             <label htmlFor='password'> Password </label>
-                            <input type="password" name='password' id='' required />
+                            <input onBlur={passField} type="password" name='password' id='2' required />
                         </div>
                         <input className='form-submit' type="submit" value="Login" required />
                     </form>
+                    <p style={{color: 'red'}}>
+                        {error?.message}
+                    </p>
                     <p>
                         New to Eha-John? <Link to='/signup' className='form-link'> Create New Account </Link>
                     </p>
-                    <p> __________________________  or  __________________________ </p>
+                    <p> ______________________________________________________ </p>
+                    <button> <i className="fa-brands fa-google "></i> Continue With Google </button>
                 </div>
             </div>
         </div>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -16,9 +16,9 @@ const SignUp = () => {
     let [conpass, setConpass] = useState('');
     let [error, setError] = useState('');
 
-    // let [createUserWithEmailAndPassword ] = useCreateUserWithEmailAndPassword(auth);
+    // let [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
 
-    let emailFild = event =>{
+    let emailFild = event => {
         setEmail(event.target.value)
         // console.log(email);
     }
@@ -33,24 +33,28 @@ const SignUp = () => {
         event.preventDefault();
 
         createUserWithEmailAndPassword(auth, email, pass)
-        // createUserWithEmailAndPassword( email, pass)
-        .then( (userCredential) => {
-            const myuser = userCredential
-            console.log(myuser);
-            if(myuser) {
-                navigation('/login')
-            }
-        })
-        .catch(err => {
-            console.error(err);
-        })
+        // createUserWithEmailAndPassword(email, pass)
+            .then((userCredential) => {
+                const myuser = userCredential.user;
+                console.log(myuser);
+                if (myuser) {
+                    navigation('/login')
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            })
 
-        if(pass !== conpass) {
+        if (pass !== conpass) {
             setError("Confirm Password Don't Match")
             return;
         }
+
+        // if (user) {
+        //     navigation('/login')
+        // }
     }
-    
+
 
     return (
         <div className='form-container'>
@@ -72,13 +76,13 @@ const SignUp = () => {
                         </div>
                         <input className='form-submit' type="submit" value="Sign Up" required />
                     </form>
-                    <p style={{color: 'red'}}>
+                    <p style={{ color: 'red' }}>
                         {error}
                     </p>
                     <p>
                         Already Have Account ? <Link to='/login' className='form-link'> Login </Link>
                     </p>
-                    <p> __________________________  or  __________________________ </p>
+                    <p> ______________________________________________________ </p>
                     <button> <i className="fa-brands fa-google "></i> Continue With Google </button>
                 </div>
             </div>
